3898bd22428d5dc3ef4a3943c611bea8b40c7d0f
Anton <68000966+Kri0k@users.noreply.github.com>
2026-07-23 20:05:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@
                 entry.target.classList.add('visible');
             }
         });
-    }, { threshold: 0.2, rootMargin: "0px 0px -40px 0px" });
+    }, { threshold: 0.2, rootMargin: "0px 0px -10% 0px" });
       
     revealElements.forEach(el => observer.observe(el));
       
